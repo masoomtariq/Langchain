34ec3ab765fea67bf09d28ead27590e17fd8f0d3
Masoom Tariq <167184654+masoomtariq@users.noreply.github.com>
2025-01-13 04:07:04 +0000

    update

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,8 +1,11 @@
 from langchain.prompts import ChatPromptTemplate
 from langchain.output_parsers import RetryOutputParser
 
+from dotenv import load_dotenv
 import os
-from import load_dotenv
+
+load_dotenv()  # Add this line to load environment variables from .env file
+
 
 ## Langmith tracking
 os.environ["LANGCHAIN_TRACING_V2"]="true"
