d41071e32a93f77f1a4ed51cee4d19ec59f31767
Masoom Tariq <167184654+masoomtariq@users.noreply.github.com>
2025-01-13 08:02:36 +0000

    Add Mistral API integration and update environment variables

--- chatbot/app.py
+++ chatbot/app.py
@@ -3,6 +3,24 @@ from langchain.output_parsers import RetryOutputParser
 
 from dotenv import load_dotenv
 import os
+import os
+from mistralai import Mistral
+
+api_key = os.environ["MISTRAL_API_KEY"]
+model = "mistral-large-latest"
+
+client = Mistral(api_key=api_key)
+
+chat_response = client.chat.complete(
+    model= model,
+    messages = [
+        {
+            "role": "user",
+            "content": "What is the best French cheese?",
+        },
+    ]
+)
+print(chat_response.choices[0].message.content)
 
 load_dotenv()  # Add this line to load environment variables from .env file
 
