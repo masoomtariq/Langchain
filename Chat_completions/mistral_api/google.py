de284a82393639e2918ff33b53ff6aa94b530e46
Masoom Tariq <167184654+masoomtariq@users.noreply.github.com>
2025-01-14 10:48:51 +0000

    Refactor chatbot implementation: replace old goo.py with updated google.py, modify .env variables, and enhance functionality

--- Chat_completions/mistral_api/google.py
+++ Chat_completions/mistral_api/google.py
@@ -7,6 +7,13 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["GOOGLE_API_KEY"]=os.getenv("GOOGLE_API_KEY")
+
 st.title("Chatbot Using Google api")
 
 prompt=ChatPromptTemplate.from_messages(
@@ -19,21 +26,33 @@ prompt=ChatPromptTemplate.from_messages(
 
 generation_config = {"temperature": 0 }
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+'''model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash-8b",
+  generation_config=generation_config,
+)'''
 
-llm = ChatGoogleGenerativeAI(model_name="gemini-1.5-flash-8b")
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash-8b",
+    api_key=os.getenv("GOOGLE_API_KEY"),
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+)
 #generation_config=generation_config)
 
 st.subheader("Enter your prompt")
 
 input_text = st.text_input("Enter: ")
-#parser = PydanticOutputParser(pydantic_object=input_text)
 
-cahin = prompt | model# | parser
+parser = PydanticOutputParser(pydantic_object=input_text)
 
-#out = chain.invoke()
+chain = prompt | llm
+
+out = chain.invoke({"question":input_text})
 
 if input_text:
-    st.write(chain.invoke({"question":input_text}))
+    st.write(parser.invoke(out))
+    #st.write(chain.run({"question": input_text}))
 
 
 #st.write(parser.invoke({"query": state["query"]}))
